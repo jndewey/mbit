df5bb0021d6b30559bae81f4e86f4eb31529d266
HKLab <jndewey@gmail.com>
2020-05-28 07:38:36 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
 /// <reference no-default-lib="true"/>
-bluetooth.startIOPinService()
 
 /**
 
-led.plot(0, 0)
+icons.useExistingIcon("")
  * Easier to use icon library.
  */
 //% color=#007EF4 weight=96 icon="\uf294"
@@ -14,8 +13,8 @@ namespace icons {
     *
     */
     //%  @param icon_name, e.g. owl
-    //% blockId=use_existing_icon block="use existing icon %icon_name" blockGap=8
-    export function useExistingIcon(icon_name: string): void {
+    //% blockId=use_existing block="use existing icon %icon_name" blockGap=8
+    export function useExistingIco(icon_name: string): void {
         console.log(icon_name)
     }
 
