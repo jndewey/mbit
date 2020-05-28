024b7b3e122706bb771752e23aacfb558c7770f8
HKLab <jndewey@gmail.com>
2020-05-28 12:49:54 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -1,22 +1,26 @@
 /// <reference no-default-lib="true"/>
-bluetooth.startIOPinService()
 
 /**
 
-led.plot(0, 0)
  * Easier to use icon library.
  */
 //% color=#007EF4 weight=96 icon="\uf294"
 namespace icons {
-
+    enum iconList {
+        Cat,
+        Dog,
+        Turtle,
+        Fox
+    };
     /**
     * Use existing icon
     *
     */
     //%  @param icon_name, e.g. owl
-    //% blockId=use_existing_icon block="use existing icon %icon_name" blockGap=8
-    export function useExistingIcon(icon_name: string): void {
-        console.log(icon_name)
+    //% blockId=use_existing_icon block="use existing icon %e" blockGap=8
+    export function useExistingIcon(e: iconList): void {
+        const thing = e;
+        console.log(e.toString())
     }
 
 }
