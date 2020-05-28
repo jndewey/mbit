f946ca89f24bbf69b6a560f3d9cbca3561526d32
HKLab <jndewey@gmail.com>
2020-05-28 02:05:15 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/// <reference no-default-lib="true"/>
+/**
+ * Easier to use icon library.
+ */
+//% color=#007EF4 weight=96 icon="\uf294"
+namespace icons {
+
+    /**
+    *  Use existing icon
+    */
+    //% blockId=use_existing_icon block="use existing icon" blockGap=8
+    export function useExistingIcon(icon_name: string): void {
+        console.log(icon_name)
+    }
+
+}
