ed4d1c977ef8ad4f68dee18722dbe3c7b4a8107e
HKLab <jndewey@gmail.com>
2020-05-28 02:19:32 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@
 bluetooth.startIOPinService()
 
 /**
+
+led.plot(0, 0)
  * Easier to use icon library.
  */
 //% color=#007EF4 weight=96 icon="\uf294"
@@ -12,7 +14,7 @@ namespace icons {
     *
     */
     //%  @param icon_name, e.g. owl
-    //% blockId=use_existing_icon block="use existing icon" blockGap=8
+    //% blockId=use_existing_icon block="use existing icon %icon_name" blockGap=8
     export function useExistingIcon(icon_name: string): void {
         console.log(icon_name)
     }
